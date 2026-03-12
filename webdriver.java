3e6a30073516d6daad1b4c2a789a26a70a2eece7
mohanprabhu2401-oss <mohanprabhu2401@gmail.com>
2026-03-12 11:51:33 +0530

    Update webdriver.java

--- webdriver.java
+++ webdriver.java
@@ -1,11 +1,8 @@
 package seleniumBasicCode;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class webdriver {
 
